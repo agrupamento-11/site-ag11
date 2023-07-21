8d3ea2858dadf6f3dc724275271e42f62a49a242
cabanga <manucabanga@gmail.com>
2023-07-21 12:32:25 +0100

    this commit Config Assets

--- src/app/views/site/home/home.component.ts
+++ src/app/views/site/home/home.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss']
 })
+
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+    scouts: any = []
+    url: string = '/assets/scouts.json';
 
-  ngOnInit(): void {
-  }
+
+    constructor(
+        private http: HttpClient
+    ) {
+        this.loading_data()
+    }
+
+    ngOnInit(): void {
+        
+    }
+
+
+    loading_data(){
+        this.scouts = this.data_scouts()
+    }
+
+
+    data_scouts(){
+        this.http.get(this.url)
+        .subscribe((res) => {
+            this.scouts = res;
+        }) 
+    } 
+
+
+
+
+
+
+
+
 
 }
