f59117df831e0326486aa497c8185087ac0621b2
cabanga <manucabanga@gmail.com>
2025-02-27 14:56:53 +0100

    This commit updete site - Navbar

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
     production: false,
     baseURL: 'https://qas.moxilan.com/',
     baseVersion: 'v1',
-    fullBaseUrl: `https://liz.agrupamento11.com/api/v1`,
+    fullBaseUrl: `https://liz.agrupamento11.org/api/v1`,
     invoiceServe: {
         baseUrl: 'https://print.kambandu.com/api/v1/document',
         output: 'generate?output_format=pdf'
